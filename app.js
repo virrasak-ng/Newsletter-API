731db359da37fc3a0b775797fd9d4425119675af
VirrasakNg <virrasaknkm.99@gmail.com>
2023-07-01 18:22:39 +0800

    Latest Update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 
